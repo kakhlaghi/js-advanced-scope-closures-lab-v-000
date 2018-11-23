04ebcafa781d0c3efa6aaa05a67fab9deff7dd91
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-23 23:50:21 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+function produceDrivingRange(blockRange){
+  return function(blockStart, blockEnd){
+    let start = parseInt(blockStart);
+    let end = parseInt(blockEnd);
+    let distance = Math.abs(start-end);
+    let rangeCheck = blockRange - distance;
+
+    if rangeCheck > 0
+  }
+}
