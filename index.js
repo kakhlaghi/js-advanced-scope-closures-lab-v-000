0098a97dee9b015672622e939034b82baab9fe76
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-23 23:56:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function produceDrivingRange(blockRange){
     if (rangeCheck > 0){
       return true
     } else {
-      return `${rangeCheck} out of range`
+      return `${distance} blocks out of range`
     }
   }
 }
